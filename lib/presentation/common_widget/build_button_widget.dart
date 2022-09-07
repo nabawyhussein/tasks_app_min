75338fe5ee238f434b37a95d149181f8870cb012
Nabawy Hussein <37081451+nabawyhussein@users.noreply.github.com>
2022-09-07 03:24:54 +0200

    CreateTaskScreen

--- lib/presentation/common_widget/build_button_widget.dart
+++ lib/presentation/common_widget/build_button_widget.dart
@@ -8,25 +8,30 @@ import '../resources/color_manger.dart';
 class BuildButtonWidget extends StatelessWidget {
   const BuildButtonWidget({
     Key? key,
-    this.txt,
+    required this.txt,
     this.onPressed,
   }) : super(key: key);
-  final String? txt;
+  final String txt;
   final VoidCallback? onPressed;
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        primary: ColorManger.darkGrey,
-        minimumSize: Size(AppSize.size(context).width * .65,
-            AppSize.size(context).height * .056),
-        //padding: EdgeInsets.symmetric(horizontal: AppSize.size(context).width * .25,vertical: 10),
+
+      style: ButtonStyle(
+
+          backgroundColor: MaterialStateProperty.all(ColorManger.primary),
+          shape:  MaterialStateProperty.all(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(   AppSize.size(context).width*0.03,),
+              // side: BorderSide(color: ColorManger.primary),
+            ),
+          )
       ),
       onPressed: onPressed,
-      child: Text(txt!,
+      child: Text(txt,
           style: TextStyle(
-              color: ColorManger.error,
+              color:Colors.white,
               fontSize: AppSize.size(context).width * .045)),
     );
   }
